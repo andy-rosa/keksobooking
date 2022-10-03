a61249ceef9933af4ac8b13115885bf9360ef7d8
Andy <roshitu@yandex.ru>
2022-10-04 00:59:37 +0300

    Дорабатывает функции по примерам из задания 4.14

--- js/main.js
+++ js/main.js
@@ -1,52 +1,60 @@
 const isPositive = (number) => number >= 0;
 
-const getRandomRange = (minValue, maxValue) => {
-  let result = Math.round(Math.random() * (maxValue - minValue) + minValue);
+const getRandomNumberInRange = ((lower, upper) => {
+  const result = Math.round(Math.random() * (upper - lower) + lower);
+  return Math.floor(result);
+});
 
-  if (result < minValue && result++ > maxValue) {
-    return `Диапозон от ${minValue} до ${maxValue} не содержит целых чисел`;
-  } else if (result < minValue) {
-    return result++;
-  } else if (result > maxValue) {
-    return result--;
-  }
-  return result;
-};
-
-const getIntegerRange = (firstValue, secondValue) => {
+/**
+ *
+ * @param {number} firstValue Начальное значение диапазона
+ * @param {number} secondValue Конечное значение диапозона
+ * @returns  Возвращающает случайное целое число из переданного диапазона включительно.
+ */
+const getRandomPositiveInteger = (firstValue, secondValue) => {
   if (isPositive(firstValue) && isPositive(secondValue)) {
+
     switch (firstValue === secondValue) {
       case true:
         if (firstValue === Math.round(firstValue)) {
           return firstValue;
         }
         return `Диапозон от ${firstValue} до ${secondValue} не содержит целых чисел`;
-      default:
-        if (firstValue > secondValue) {
-          const swap = firstValue ;
-          firstValue = secondValue;
-          secondValue = swap;
+
+      default: {
+        const lower = Math.ceil(Math.min(firstValue, secondValue));
+        const upper = Math.floor(Math.max(firstValue, secondValue));
+        if (lower > upper) {
+          return `Диапозон от ${firstValue} до ${secondValue} не содержит целых чисел`;
         }
-        return getRandomRange(firstValue, secondValue);
+
+        return getRandomNumberInRange(lower, upper);
+      }
     }
+
   }
   return NaN;
 };
 
-getIntegerRange(1, 2);
-
-
-function getFloatRandomRange(firstValue, secondValue, floatPointValue) {
+/**
+ *
+ * @param {number} firstValue Начальное значение диапазона
+ * @param {number} secondValue Конечное значение диапозона
+ * @param {number} floatPointValue Количество знаков после запятой по умолчанию: 1
+ * @returns Возвращающает случайное число с плавающей точкой из переданного диапазона включительно
+ */
+function getRandomPositiveFloat(firstValue, secondValue, floatPointValue = 1) {
   if (isPositive(firstValue) && isPositive(secondValue) && isPositive(floatPointValue) && firstValue !== secondValue) {
-    if (firstValue > secondValue) {
-      const swap = firstValue ;
-      firstValue = secondValue;
-      secondValue = swap;
-    }
-    const result = Math.random() * (secondValue - firstValue) + firstValue;
-    return result.toFixed(floatPointValue);
+
+    const lower = Math.min(firstValue, secondValue);
+    const upper = Math.max(firstValue, secondValue);
+
+    const result = Math.random() * (upper - lower) + lower;
+    return +result.toFixed(floatPointValue);
   }
+
   return NaN;
 }
 
-getFloatRandomRange(2.324, 0.1234324234, 3);
+getRandomPositiveInteger(1, 2);
+getRandomPositiveFloat(2.324, 0.1234324234, 3);
