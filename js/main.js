ea30727fe37c4430688fc60def44a43fb6b19967
Andy <roshitu@yandex.ru>
2022-10-13 20:58:59 +0300

    Fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-import './fill-template.js';
+import './fill-data/fill-template.js';
 import { activateForm, deactivateForm } from './toggle-state-page.js';
+import { fillTemplate } from './fill-data/fill-template.js';
 
+fillTemplate();
 activateForm();
 deactivateForm();
