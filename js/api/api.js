cf27bc4a7148f4bea2c92779255ed7442dec2d1c
Andy <roshitu@yandex.ru>
2022-11-02 20:24:07 +0300

    Активирует форму когда данные с сервера были успешно загружены

--- js/api/api.js
+++ js/api/api.js
@@ -1,6 +1,7 @@
 import { showErrorMapMessage } from '../error/map-error.js';
 import { showErrorFormMessage } from '../error/form-post-error.js';
 import { resetForm } from '../utils/reset-form.js';
+import { activateFilter, deactivateFilter } from '../toggle-state-page.js';
 
 const Url = {
   GET: 'https://27.javascript.pages.academy/keksobooking/data',
@@ -11,14 +12,19 @@ const getAdsDataServer = (onSuccess) => {
   fetch(Url.GET)
     .then((response) => {
       if (response.ok) {
+        activateFilter();
         return response.json()
           .then((ads) => onSuccess(ads));
 
       } else {
         showErrorMapMessage();
+        deactivateFilter();
       }
     })
-    .catch(() => showErrorMapMessage());
+    .catch(() => {
+      deactivateFilter();
+      showErrorMapMessage();
+    });
 };
 
 const sendFormData = (onSuccess, body) => {
