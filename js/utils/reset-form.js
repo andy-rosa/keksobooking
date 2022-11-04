962095ec978ae8d3130eeb7995155aa1a7d12236
Andy Rosa <andy.roshitu@gmail.com>
2022-11-04 18:03:47 +0300

    Синхронизирует тип жилья и плейсхолдер его минимальной цены

--- js/utils/reset-form.js
+++ js/utils/reset-form.js
@@ -1,12 +1,14 @@
 import { setStartPosition, START_COORDINATES, mainPinMarker } from '../map/main-pin.js';
+import { typeMinPrice } from '../utils.js';
 
 const form = document.querySelector('.ad-form');
 const filter = document.querySelector('.map__filters');
-
+const priceInput = form.querySelector('#price');
 
 const resetForm = () => {
   filter.reset();
   form.reset();
+  priceInput.setAttribute('placeholder', typeMinPrice['bungalow'] );
   setStartPosition(START_COORDINATES);
   mainPinMarker.setLatLng(START_COORDINATES);
 };
