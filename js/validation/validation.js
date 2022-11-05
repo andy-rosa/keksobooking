62978fb8aac52840e410af4558eb41c5f0bd241f
Andy Rosa <andy.roshitu@gmail.com>
2022-11-05 08:02:55 +0300

    Доработка сброса/отправки формы по тех.заданию

--- js/validation/validation.js
+++ js/validation/validation.js
@@ -5,6 +5,7 @@ import { onTimeChange, fieldsetTime } from './check-time.js';
 import { sendFormData } from '../api/api.js';
 import { showSuccessMessage } from './success-form-message.js';
 import { blockSubmitButton, unblockSubmitButton } from '../submit-button.js';
+import { resetForms } from '../utils/reset.js';
 
 const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
@@ -16,20 +17,26 @@ const pristine = new Pristine(form, {
 pristine.addValidator(roomSelect, validateRoom, 'Не подходящее значение');
 pristine.addValidator(priceInput, validatePrice, 'Цена меньше минимальной');
 
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+const setAdFormSubmit = (cb, callbackArgument) => {
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
 
-  const isValid = pristine.validate();
-  if (isValid) {
-    blockSubmitButton();
-    sendFormData(() => {
-      showSuccessMessage();
-      unblockSubmitButton();
-    },
-    new FormData(evt.target)
-    );
-  }
-});
+    const isValid = pristine.validate();
+    if (isValid) {
+      blockSubmitButton();
+      sendFormData(() => {
+        showSuccessMessage();
+        resetForms();
+        cb(callbackArgument);
+        unblockSubmitButton();
+      },
+      new FormData(evt.target)
+      );
+    }
+  });
+};
 
 fieldsetTime.addEventListener('change', onTimeChange);
 typeSelect.addEventListener('change', onTypeChange);
+
+export { setAdFormSubmit };
